7050162afa71332565d3bbbedbddfb433db1331d
Victorpraise5 <victorpraise949@gmail.com>
2024-01-23 11:55:10 -0800

    task5

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Square Module """
+
+
+class Square:
+    """ Square Class """
+
+    def __init__(self, size=0):
+        """ Initializes the data """
+        self.size = size
+
+    @property
+    def size(self):
+        """ Returns the size of a square instance """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Sets the size of a Square instance """
+        if isinstance(value, int):
+            if value >= 0:
+                self.__size = value
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """ Calculates de square's area """
+        return self.__size ** 2
