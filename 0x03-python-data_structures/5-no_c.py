74d8c9c707940d9ef70f7288b50d8a0018bb8dba
Victorpraise5 <victorpraise949@gmail.com>
2024-01-24 23:58:15 -0800

    task6

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     result_string = " "
     for char in my_string:
-        if char.lower() != 'c':
+        if char != 'c' and char != 'C':
             result_string += char
     return result_string
