3f788ed27881d4eb365365edf1a16a7f39ec25b0
Victorpraise5 <victorpraise949@gmail.com>
2024-04-10 01:48:28 -0700

    String representation

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""rectangle module"""
+
+class Rectangle:
+    """rectangle class"""
+
+    def __init__(self, width=0, height=0):
+        """initializing data"""
+        self.width =  width
+        self.height = height
+
+    @property
+    def width(self):
+        """to retrieve width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """to set it"""
+        if isinstance(value, int):
+            if value >=0:
+                self.__width = value
+            else:
+                raise ValueError("width must be >=0")
+        else:
+            raise TypeError("width must be an integer")
+
+    @property
+    def height(self):
+        """to retrieve the height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """to set the height"""
+        if isinstance(value, int):
+            if value >=0:
+                self.__height = value
+            else:
+                raise ValueError("height must be >=0")
+        else:
+            raise TypeError("height must be an integer")
+
+    def area(self):
+        """returns the rectangle area"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """returns the rectangle perimeter"""
+        if self.__width == 0 and self.__height == 0:
+            perimeter = 0
+        else:
+            perimeter = (self.__width + self.__height) * 2
+        return perimeter
+
+    def __str__(self):
+        """to provide a string representation"""
+        result = ""
+        if self.__width == 0 or self.__height == 0:
+            return result
+        else:
+            for i in range(self.__height):
+                result += "#" * self.__width + "\n"
+            return result.strip()
