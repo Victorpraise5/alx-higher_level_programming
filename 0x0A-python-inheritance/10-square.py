142eb1259d5b82946c31fdffa602f7621fb59ebb
Victorpraise5 <victorpraise949@gmail.com>
2024-04-16 04:56:15 -0700

    Square #1

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""square module"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """Square class"""
+
+    def __init__(self, size):
+        """initializing data"""
+        super().__init__(size, size)
+        self.__size = size
+        self.integer_validator("size", size)
+
+    def area(self):
+        """to implement the area"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """string representation of the sqaure"""
+        return f"[Square] {self.__size}/{self.__size}"
