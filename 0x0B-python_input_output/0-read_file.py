76802ff88d27c62a438de18a52bb9f6347b5a9cb
Victorpraise5 <victorpraise949@gmail.com>
2024-04-23 04:35:05 -0700

    Read File2

--- 0x0B-python_input_output/0-read_file.py
+++ 0x0B-python_input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""Read file module"""
+""" Read File Module """
+
 
 def read_file(filename=""):
-    """function that reads a text file(UTF-8) and prints it to stdout"""
-    with open(filename, mode='r', encoding="utf-8") as my_file:
+    """ Function reads a text file(UTF-8) and prints it to stdout """
+    with open(filename, mode="r", encoding="utf-8") as my_file:
         print(my_file.read(), end='')
