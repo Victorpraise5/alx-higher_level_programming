9f3ae33f4ea13c28fcee873c512c82c5aaf8e610
Victorpraise5 <victorpraise949@gmail.com>
2024-06-20 06:11:12 -0700

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,13 +1,9 @@
 #!/usr/bin/node
-
-const args = process.argv.slice(2);
-
-const numArgs = args.length;
-
-if (numArgs === 0){
-	console.log('No argument');
-}else if (numArgs === 1){
-	console.log('Arguments found');
-}else {
-	console.log('Arguments found');
+const args = process.argv;
+if (args.length < 3) {
+  console.log('No argument');
+} else if (args.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
