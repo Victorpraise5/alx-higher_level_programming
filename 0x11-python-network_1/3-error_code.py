38cf219cd0b31eecc627459ba1cffcdbc528ed9c
Victorpraise5 <victorpraise949@gmail.com>
2024-07-05 07:08:20 -0700

    Error code #0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """
-script takes in a URL, sends a request to the URL and displays
-the body of the response (decoded in utf-8_
+script takes in a URL, sends a request to the URL and
+displays the body of the response (decoded in utf-8)
 """
 from urllib.request import urlopen, Request
 from urllib.error import HTTPError
 from sys import argv
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     req = Request(argv[1])
 
     try:
